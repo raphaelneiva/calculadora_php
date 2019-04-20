0710db427775d392c09d0e6e32721f70c144c204
Raphael Neiva <raphaelpneiva@gmail.com>
2019-04-20 09:47:06 -0300

    backend finalizado

--- calculadora.php
+++ calculadora.php
@@ -1,3 +1,25 @@
+<?php
+    $res = 0;
+    if ( count($_POST) && isset ($_POST["op"])) {
+        switch ( $_POST["op"] ){
+            case "som" :
+                $res = $_POST["n1"] + $_POST["n2"];
+                break;
+            case "sub" :
+                $res = $_POST["n1"] - $_POST["n2"];
+                break;
+            case "div" :
+                $res = $_POST["n1"] / $_POST["n2"];
+                break;
+            case "mul" :
+                $res = $_POST["n1"] * $_POST["n2"];
+                break;
+
+        }
+    }
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,6 +33,47 @@
 </head>
 
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col s8">
+            <h4 class="header">Minha Calculadora</h4>
+            <div class="card horizontal">
+                <div class="card-image">
+                </div>
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <div class="row">
+                            <form method="post">
+                                <div class="input-field col s6">
+                                    <input id="n1" type="number" name="n1">
+                                    <label for="n1">Operando 1 </label>
+                                </div>
+                                <div class="input-field col s6">
+                                    <input id="n2" type="number" name="n2">
+                                    <label for="n2">Operando 2 </label>
+                                </div>
+                                <div class="input-field col s12">
+                                    <button type="submit" name="op" value="som" class="btn col s2 offset-s1 amber lighten-1"> Soma </button>
+                                    <button type="submit" name="op" value="div" class="btn col s2 offset-s1 light-blue darken-2"> Divi </button>
+                                    <button type="submit" name="op" value="mul" class="btn col s2 offset-s1 green lighten-1"> Mult </button>
+                                    <button type="submit" name="op" value="sub" class="btn col s2 offset-s1 red lighten-1"> Subt </button>
+                                </div>
+
+                            </form>
+                        </div>
+
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Resultado: <?php echo $res ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <!--JavaScript at end of body for optimized loading-->
 <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="node_modules/materialize-css/dist/js/materialize.min.js "></script>
